2505ff27a6b2ac091deb880f7c64f0725a3e6612
Alex Tomas <atomasllimos@gmail.com>
2022-07-30 09:11:20 +0200

    Add new feautures

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Importamos el AuthenticationError
 import {
   gql,
   ApolloServer,
@@ -44,6 +43,7 @@ const typeDefinitions = gql`
     allPersons(phone: YesNo): [Person]!
     findPerson(name: String!): Person
     me: User
+    allUsers: [User]!
   }
 
   type Mutation {
@@ -56,28 +56,32 @@ const typeDefinitions = gql`
     editNumber(name: String!, phone: String!): Person
     createUser(username: String!): User
     login(username: String!, password: String!): Token
+    addAsFriend(name: String!): User
   }
 `
 
 const resolvers = {
   Query: {
     personCount: () => Person.collection.countDocuments(),
-    allPersons: async (root, args) => {
-      if (!args) return await Person.find({})
+    allPersons: async (root, { phone }) => {
+      if (!phone) return await Person.find({})
 
-      return Person.find({ phone: { $exists: args.phone === 'YES' } })
+      return await Person.find({ phone: { $exists: phone === 'YES' } })
     },
-    findPerson: (root, args) => {
+    findPerson: async (root, args) => {
       const { name } = args
-      return Person.findOne({ name })
+      return await Person.findOne({ name })
     },
-    // Como tercer parametro tenemos el valor que retorna el context
     me: (root, args, context) => {
       return context.currentUser
+    },
+    allUsers: async (root, args, { currentUser }) => {
+      if (!currentUser) throw new AuthenticationError('Not Authenticated')
+      const users = await User.find({})
+      return users
     }
   },
   Mutation: {
-    // Modificamos el addPerson para que solo los usuarios logeados puedan crear una persona
     addPerson: async (root, args, context) => {
       const { currentUser } = context
       if (!currentUser) throw new AuthenticationError('Not Authenticated')
@@ -85,9 +89,6 @@ const resolvers = {
 
       try {
         await person.save()
-        // Añadimos a los amigos del usuario registrado la persona que ha creado
-        currentUser.friends = currentUser.friends.concat(person)
-        await currentUser.save()
       } catch (e) {
         throw new UserInputError(error.message, {
           invalidArgs: args
@@ -131,6 +132,19 @@ const resolvers = {
       return {
         value: jwt.sign(userForToken, JWT_SECRET)
       }
+    },
+    addAsFriend: async (root, args, { currentUser }) => {
+      if (!currentUser) throw new AuthenticationError('Not Authenticated')
+      const person = await Person.findOne({ name: args.name })
+      if (!person)
+        throw new UserInputError(`Cannot find ${args.name} in database`)
+      const nonFriendlyAlredy = (person) =>
+        !currentUser.friends.map((p) => p._id).includes(person._id)
+      if (!nonFriendlyAlredy(person)) {
+        currentUser.friends = currentUser.friends.concat(person)
+        await currentUser.save()
+      }
+      return currentUser
     }
   },
   Person: {
@@ -146,22 +160,15 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: typeDefinitions,
   resolvers,
-  // Creamos el context y recibimos como parametro la request, cada vez que hagamos una request pasara por esta funcions
   context: async ({ req }) => {
-    // En auth vamos a ver si tenemos una req guardaremos el req.headers.authorization y si no sera igual a null
     const auth = req ? req.headers.authorization : null
 
-    // Comprobamos que auth no es null y que empieza por bearer que es como le vamos a pasar el token
     if (auth && auth.toLocaleLowerCase().startsWith('bearer ')) {
-      // Extraemos el token que estara despues de bearer por eso el substring(7)
       const token = auth.substring(7)
-      // Verificamos el token si es correcto tendremos el userForToken con el username y la id
       const { id } = jwt.verify(token, JWT_SECRET)
 
-      // Buscamos el usuario en nuestra base de datos por id y le decimos que nos traiga tambien la informacion de los amigos
       const currentUser = await User.findById(id).populate('friends')
 
-      // Retornamos el currentUser como objeto
       return { currentUser }
     }
   }
